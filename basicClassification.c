f5e1ba23ebe4706018fc16f879d9ac0264db857e
maorsaadon <105555708+maorsaadon@users.noreply.github.com>
2022-11-23 19:33:42 +0200

    Add files via upload

--- basicClassification.c
+++ basicClassification.c
@@ -2,6 +2,7 @@
 
 
 int isPrime(int num){
+    if (num == 0) return 0;
     int square = sqrt(num);
     for (int i = 2; i <= square; i++)
         if(num % i == 0) return 0;
@@ -12,6 +13,7 @@ int factorial(int num){
     return num*factorial(num-1);
 }
 int isStrong(int num){
+    if (num == 0) return 0;
     int sum = 0;
     for (int i = num; i > 0; i = i/10)
         sum += factorial(i % 10);
