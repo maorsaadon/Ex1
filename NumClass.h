89743ea02c50e96f63a4e637cb67e05d9f620f27
maorsaadon <105555708+maorsaadon@users.noreply.github.com>
2022-11-24 19:01:42 +0200

    Add files via upload

--- NumClass.h
+++ NumClass.h
@@ -5,5 +5,10 @@ int isArmstrong(int num);
 int isPalindrome(int num);
 int isPrime(int num);
 int isStrong(int num);
+int factorial(int num);
+int isPrime(int num);
+int NumberLength(int num);
+int isPalindromeReverse(int num , int ReverseNum);
+int isArmstongLength(int num, int length);
 
 #endif 
